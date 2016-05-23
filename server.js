bf2b50bb3522e4289120038568d9ddac89d62370
Cynthia Salinas <csalinas123@gmail.com>
2016-05-23 15:37:02 -0700

    add package.json and procfile

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+var http = require('http'); 
 
+var server = http.createServer (function(req, res){
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end ('<h1>Check Update Status</h1');
 
+});
 
+var port = Number(process.env.PORT || 3000);
 
-
-
-app.listen(process.env.PORT || 3000);
+server.listen(port);
