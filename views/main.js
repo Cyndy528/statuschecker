024f9f3b6de4e35e008436b29b83899f66b07477
Cynthia Salinas <csalinas123@gmail.com>
2016-06-01 21:19:48 -0700

    add server.js on procfile

--- views/main.js
+++
@@ -1,9 +0,0 @@
-var http = require('http'); 
-var port = process.env.PORT || 3000; 
-
-http. createServer(function(request, response){
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('Status Update Checker');
-}).listen(port, function() {
-	console.log("Server Running");
-});
