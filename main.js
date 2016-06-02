1e156fe35114b6c6b7c2d561b0dfa0ea1e275d53
Cynthia Salinas <csalinas123@gmail.com>
2016-06-01 20:26:22 -0700

    remove host name

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ var port = process.env.PORT || 3000;
 http. createServer(function(request, response){
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.end('Status Update Checker');
-}).listen(port, hostname, function() {
+}).listen(port, function() {
 	console.log("Server Running");
 });
